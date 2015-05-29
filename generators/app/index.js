8ec43edd1a67dc9161cb9dbf58113cac26b150f0
Philipp Alferov <philipp.alferov@gmail.com>
2015-05-28 23:48:47 -0400

    Refactoring

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,65 +5,42 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 
 var OnepageGenerator = yeoman.generators.Base.extend({
+  constructor: function() {
+    yeoman.generators.Base.apply(this, arguments);
+  },
   promptUser: function() {
     var done = this.async();
 
-    // have Yeoman greet the user
-    console.log(this.yeoman);
+    //Welcome message
+    if (!this.options['skip-welcome-message']) {
+      this.log(require('yosay')());
+      this.log(chalk.magenta(
+        'Out of the box I include Gulp, Browser Sync, Sass' +
+        'Main Bower Files to include all bower dependencies to build your app.'
+      ));
+    }
 
-    var prompts = [{
-      name: 'appName',
-      message: 'What is your app\'s name ?'
-    }, {
-      type: 'confirm',
-      name: 'addDemoSection',
-      message: 'Would you like to generate a demo section ?',
-      default: true
-    }];
+    // var prompts = [{
+    //   name: 'appName',
+    //   message: 'What is your app\'s name ?'
+    // }];
+    //
+    // this.prompt(prompts, function(props) {
+    //   this.appName = props.appName;
+    //   done();
+    // }.bind(this));
 
-    this.prompt(prompts, function(props) {
-      this.appName = props.appName;
-      this.addDemoSection = props.addDemoSection;
-
-      done();
-    }.bind(this));
+    done();
   },
   scaffoldFolders: function() {
     this.mkdir("app");
-    this.mkdir("app/css");
-    this.mkdir("app/sections");
-    this.mkdir("build");
+    this.mkdir("app/assets");
   },
-  copyMainFiles: function() {
-    this.copy("_footer.html", "app/footer.html");
-    this.copy("_gulpfile.js", "gulpfile.js");
-    this.copy("_package.json", "package.json");
-    this.copy("_main.css", "app/css/main.css");
-
-    var context = {
-      site_name: this.appName
-    };
-
-    this.template("_header.html", "app/header.html", context);
-  },
-  enerateDemoSection: function() {
-    if (this.addDemoSection) {
-      var done = this.async();
-      this.invoke("starter-pack:section", {
-        args: ["Demo Section"]
-      }, function() {
-        done();
-      });
-    } else {
-      this.write("app/menu.html", "");
-    }
-  },
-  runNpm: function() {
-    var done = this.async();
-    this.npmInstall("", function() {
-      console.log("\nEverything Setup !!!\n");
-      done();
-    });
+  copyFiles: function() {
+    this.fs.copyTpl(
+      this.templatePath('_index.html'),
+      this.destinationPath('index.html')
+    );
   }
 });
 
