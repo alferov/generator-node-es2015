88f4cb9b97b97cc5181ec01fc8a06dab9a522450
Philipp Alferov <philipp.alferov@gmail.com>
2015-05-30 21:36:37 -0400

    Added basic gulp operations on assets

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,1 +1,79 @@
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var mainBowerFiles = require('main-bower-files');
+var path = {
+  index: 'index.html',
+  assets: 'assets',
+  root: '.',
+  get sass() {
+    return this.assets + '/scss'
+  },
+  get js() {
+    return this.assets + '/js'
+  },
+  get dist() {
+    return this.assets + '/dist'
+  },
+  get distCss() {
+    return this.dist + '/css'
+  },
+  get distJs() {
+    return this.dist + '/js'
+  },
+};
+
+gulp.task('process-styles', function() {
+  return gulp.src(path.sass + '/custom.scss')
+  .pipe(plugins.plumber())
+  .pipe(plugins.sass())
+  .pipe(gulp.dest(path.distCss));
+});
+
+gulp.task('process-scripts',  function() {
+  return gulp.src(path.js + '/**/*.js')
+  .pipe(plugins.concat('main.js'))
+  .pipe(gulp.dest(path.distJs));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(
+    [path.sass + '/**/*.scss'],
+    ['styles']
+  );
+
+  gulp.watch(
+    [path.js + '/**/*.js'],
+    ['styles']
+  );
+});
+
+gulp.task('vendors', function() {
+  var vendors = gulp.src(mainBowerFiles());
+
+  return gulp.src(path.index)
+    .pipe(plugins.inject(vendors, {
+      relative: true,
+      name: 'vendors'
+    }))
+    .pipe(gulp.dest(path.root));
+});
+
+gulp.task('scripts', ['process-scripts'], function() {
+  var target = gulp.src(path.index);
+  var app = gulp.src(path.js + '/**/*.js');
+
+  return target.pipe(plugins.inject(app, {
+      relative: true,
+    }))
+    .pipe(gulp.dest(path.root));
+});
+
+gulp.task('styles', ['process-styles'], function() {
+  var target = gulp.src(path.index);
+  var app = gulp.src(path.distCss + '/**/*.css');
+
+  return target.pipe(plugins.inject(app, {
+      relative: true,
+    }))
+    .pipe(gulp.dest(path.root));
+});
