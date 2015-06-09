79ceb328c91b4c21ab04b63f50b2cc252f03e858
Philipp Alferov <philipp.alferov@gmail.com>
2015-06-09 16:23:53 -0400

    :whale: Fix gulpfile tasks flow

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -2,16 +2,18 @@
 'use strict';
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 var plugins = require('gulp-load-plugins')();
 var mainBowerFiles = require('main-bower-files');
 var runSequence = require('run-sequence');
+
 var path = {
   root: 'app',
   bower: 'bower_components',
+  assets: 'app/assets',
   get index() {
     return this.root + '/index.html';
   },
-  assets: 'app/assets',
   get sass() {
     return this.assets + '/scss';
   },
@@ -19,17 +21,32 @@ var path = {
     return this.assets + '/js';
   },
   get dist() {
-    return this.assets + '/dist';
+    return 'dist';
   },
   get distCss() {
     return this.dist + '/css';
   },
   get distJs() {
     return this.dist + '/js';
-  },
+  }
 };
 
-gulp.task('process-styles', function() {
+gulp.task('vendors', function() {
+
+  return gulp.src(mainBowerFiles('**/*.js'))
+    .pipe(plugins.concat('vendors.js'))
+    .pipe(gulp.dest(path.distJs + '/vendors'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(path.js + '/**/*.js')
+    .pipe(plugins.concat('main.js'))
+    .pipe(gulp.dest(path.distJs))
+    .pipe(reload({once: true, stream: true}));
+});
+
+gulp.task('styles', function() {
+
   return gulp.src(path.sass + '/custom.scss')
     .pipe(plugins.plumber())
     .pipe(plugins.sass({
@@ -37,49 +54,43 @@ gulp.task('process-styles', function() {
         path.bower + '/bootstrap-sass-official/assets/stylesheets',
       ]
     }))
-    .pipe(gulp.dest(path.distCss));
+    .pipe(gulp.dest(path.distCss))
+    .pipe(reload({once: true, stream: true}));
 });
 
-gulp.task('process-scripts',  function() {
-  return gulp.src([path.js + '/**/*.js', '!' + path.js + '/vendors/**/*'])
-    .pipe(plugins.concat('main.js'))
-    .pipe(gulp.dest(path.distJs));
-});
+gulp.task('templates', function() {
 
-gulp.task('vendors', function() {
-  var vendors = gulp
-  .src(mainBowerFiles('**/*.js'))
-  .pipe(gulp.dest(path.js + '/vendors'));
+  var css = gulp.src(path.distCss + '/*.css');
+  var js = gulp.src(path.distJs + '/*.js');
+  var vendors = gulp.src(path.distJs + '/vendors/*.js');
 
-  return gulp.src(path.index)
+  var target = gulp.src(path.index)
+    .pipe(gulp.dest(path.dist));
+
+  return target
+    .pipe(plugins.inject(js, {
+      relative: true,
+    }))
     .pipe(plugins.inject(vendors, {
       relative: true,
       name: 'vendors'
     }))
-    .pipe(gulp.dest(path.root));
-});
-
-gulp.task('scripts', ['process-scripts'], function() {
-  var target = gulp.src(path.index);
-  var app = gulp.src([path.distJs + '/**/*.js']);
-
-  return target.pipe(plugins.inject(app, {
+    .pipe(plugins.inject(css, {
       relative: true,
     }))
-    .pipe(gulp.dest(path.root));
+    .pipe(gulp.dest(path.dist))
+    .pipe(reload({once: true, stream: true}));
 });
 
-gulp.task('styles', ['process-styles'], function() {
-  var target = gulp.src(path.index);
-  var app = gulp.src(path.distCss + '/**/*.css');
+gulp.task('serve', ['vendors', 'scripts', 'styles'], function() {
+  browserSync.init({
+    watchTask: true,
+    debugInfo: true,
+    server: path.dist,
+    logConnections: true,
+    notify: true
+  });
 
-  return target.pipe(plugins.inject(app, {
-      relative: true,
-    }))
-    .pipe(gulp.dest(path.root));
-});
-
-gulp.task('watch', function() {
   gulp.watch(
     [path.sass + '/**/*.scss'],
     ['styles']
@@ -89,24 +100,13 @@ gulp.task('watch', function() {
     [path.js  + '/**/*.js'],
     ['scripts']
   );
+
+  gulp.watch(
+    [path.root  + '/**/*.html'],
+    ['templates']
+  );
 });
 
-gulp.task('browser-sync', function() {
-  var files = [
-    path.index,
-    path.distCss,
-    path.distJs
-  ];
+gulp.task('default', ['serve'], function() {
 
-  browserSync.init(files, {
-    watchTask: true,
-    debugInfo: true,
-    server: path.root,
-    logConnections: true,
-    notify: true
-  });
-});
-
-gulp.task('default', function(cb) {
-  runSequence('vendors', 'styles', 'scripts', 'watch', 'browser-sync', cb);
 });
